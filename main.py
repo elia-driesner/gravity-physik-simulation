295cdb17139157680406ebf5a512c59f67b243d5
elia_driesner <78803906+eliaDr@users.noreply.github.com>
2022-02-25 08:42:58 +0100

    added designs

--- main.py
+++ main.py
@@ -32,10 +32,11 @@ class Object:
         self.start_y = y
         
         if image != 0:
-            self.image = image
+            self.image = pygame.transform.scale(image, (self.width, self.height))
             
-            self.img_x = self.x + (self.width / 2)
-            self.img_y = self.y + (self.height / 2)
+            self.img_x = self.x
+            self.img_y = self.y
+            print(self.img_y)
         
         
 def calc_gravity(m1, m2, r, o1, o2, speed):
@@ -44,7 +45,7 @@ def calc_gravity(m1, m2, r, o1, o2, speed):
     t = 0.0166666667 * speed
     a = F / m2
     s = a * t * t
-    distance = round((((o1.x - o1.width) - (o2.x + o2.width)) * (pixel_meter / 300)) / 1000, 2)
+    distance = round((((o1.x) - (o2.x + (o2.width))) * (pixel_meter / 300)) / 1000, 2)
     velocity = round((s / t) * 3.6, 2)
     gravity = round(F, 2)
     
@@ -60,15 +61,14 @@ def calc_gravity(m1, m2, r, o1, o2, speed):
     return [distance, velocity, gravity, speed]
     
 
-objects.append(Object(1800, wn_size[1] / 2, 100, 600, 5.972 * 10e24, (0, 0, 255), pygame.image.load('Erth.png')))
+objects.append(Object(1100, (wn_size[1] / 2) - 300, 600, 600, 5.972 * 10e24, (0, 0, 255), pygame.image.load('Erth.png')))
 
-objects.append(Object(0, (wn_size[1] / 2) - 10, 20, 20, 60, (0, 255, 0)))
+objects.append(Object(0, (wn_size[1] / 2) - 10, 40, 40, 60, (0, 255, 0), pygame.image.load('Meteor.png')))
 
 speed = 1
 last_time = time.time()
 
 while run:  
-    print((last_time - time.time()) * -1)
     keys = pygame.key.get_pressed()
     if keys[pygame.K_PLUS] and ((last_time - time.time()) * -1) > 0.1:
         speed += 1
@@ -77,14 +77,15 @@ while run:
         if speed != 1:
             speed -= 1
             last_time = time.time()
-    wn.fill((54, 43, 49))
+    wn.fill((0, 0, 0))
     for element in objects:
         if element != objects[0]:
-            pygame.draw.rect(wn, element.color, [element.x, element.y, element.width, element.height], 0)
+            wn.blit(element.image, (element.x, element.y))
         else:
-            pygame.draw.circle(wn, element.color, (element.x, element.y), element.width)
+            # pygame.draw.circle(wn, element.color, (element.x, element.y), element.width)
+            wn.blit(element.image, (element.img_x, element.img_y))
     
-    data = calc_gravity(m1 = objects[0].mass , m2 = objects[1].mass, r =  pixel_meter * (objects[0].x - (objects[1].x + (objects[1].width / 2))), o1 = objects[0], o2 = objects[1], speed = speed)
+    data = calc_gravity(m1 = objects[0].mass , m2 = objects[1].mass, r =  pixel_meter * ((objects[0].x + (objects[0].width / 2)) - (objects[1].x + (objects[1].width / 2))), o1 = objects[0], o2 = objects[1], speed = speed)
     clock.tick(FPS)
     text = []
     pos = 0
